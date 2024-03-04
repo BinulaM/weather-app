da07724d04c0238c2f7eb1975c3e631e9c157bfb
Maksymilian Jan Matusiak <m.matusiak@se22.qmul.ac.uk>
2024-03-04 12:21:39 +0000

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './/App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1></h1>
     </div>
   );
 }
